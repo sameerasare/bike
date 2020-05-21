3dcd2e945686ca1d59e6b9a4117dbe13c4934f2d
Shriya Asare <Shriya Asare@192.168.0.9>
2020-05-21 20:24:13 +0530

    delete and view changes

--- bike/src/main/java/com/practice/user/controllers/UserControllers.java
+++ bike/src/main/java/com/practice/user/controllers/UserControllers.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.practice.user.models.User;
 import com.practice.user.repositories.UserRepository;
 
+//@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("users")
 public class UserControllers {
@@ -22,22 +25,31 @@ public class UserControllers {
 	@Autowired
 	private UserRepository userRepository;
 
+	@CrossOrigin
 	@GetMapping
 	public List<User> getUsers(){
 		return userRepository.findAll();
 	}
 	
-	
-	
-	@PostMapping
+	@CrossOrigin
+	@PostMapping("/new")
 	@ResponseStatus(HttpStatus.OK)
-	public void create(@RequestBody User bike) {
-		userRepository.save(bike);
+	public void createUser(@RequestBody User user) {
+		userRepository.save(user);
 	}
 	
+	@CrossOrigin
 	@GetMapping("/{id}")
 	public User get(@PathVariable("id") long id) {
 		return userRepository.getOne(id);
 				
+	}
+	
+	@CrossOrigin
+	@DeleteMapping(path = {"/{id}"})
+	public User deleteUser(@PathVariable("id") long id) {
+		User user = userRepository.getOne(id);
+		userRepository.deleteById(id);
+		return user;
 	}
 }
--- bike/src/main/java/com/practice/user/models/User.java
+++ bike/src/main/java/com/practice/user/models/User.java
@@ -1,16 +1,11 @@
 package com.practice.user.models;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
@@ -25,14 +20,14 @@ public class User {
 	private String type;
 	private String email;
 	private String phone;
-	private String model;
-	private String serialNumber;
-	private BigDecimal purchasePrice;
+	//private String model;
+	//private String serialNumber;
+	//private BigDecimal purchasePrice;
 	
-	@JsonFormat(shape = Shape.STRING, pattern = "MM-dd-yyyy")
-	private Date purchaseDate;
-
-	private boolean contact;
+//	@JsonFormat(shape = Shape.STRING, pattern = "MM-dd-yyyy")
+//	private Date purchaseDate;
+//
+//	private boolean contact;
 	
 	public String getName() {
 		return name;
@@ -52,41 +47,23 @@ public class User {
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	public String getModel() {
-		return model;
-	}
-	public void setModel(String model) {
-		this.model = model;
-	}
-	public String getSerialNumber() {
-		return serialNumber;
-	}
-	public void setSerialNumber(String serialNumber) {
-		this.serialNumber = serialNumber;
-	}
-	public BigDecimal getPurchasePrice() {
-		return purchasePrice;
-	}
-	public void setPurchasePrice(BigDecimal purchasePrice) {
-		this.purchasePrice = purchasePrice;
-	}
-	public Date getPurchaseDate() {
-		return purchaseDate;
-	}
-	public void setPurchaseDate(Date purchaseDate) {
-		this.purchaseDate = purchaseDate;
-	}
-	public boolean isContact() {
-		return contact;
-	}
-	public void setContact(boolean contact) {
-		this.contact = contact;
-	}
 	public long getId() {
 		return id;
 	}
 	public void setId(long id) {
 		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
 	}
 	
 
